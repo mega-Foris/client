bad0a62ad0d9f768bc5d496c059b3ad5d94d7310
Austin <joha0033@gmail.com>
2017-06-27 15:44:06 -0600

    added post page and backend connection

--- javascript/shared.js
+++ javascript/shared.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('select').material_select();
 	$('.button-collapse').sideNav();
 	$('.carousel').carousel();
+	submitForm();
 });
 
 
@@ -16,20 +17,53 @@ function callAPI(URL){
   return $.get(URL);
 }
 
+function submitForm() {
+	$("form").submit(function(event){
+	    alert("YOU CLICKED IT!")
+	    event.preventDefault();
 
-$("form").submit(function(event){
-    //alert("YOU CLICKED IT!"
-    event.preventDefault();
+	   let title = $(".title").val()
+	    let sport = $(".sport").val()
+	    var difficulty = $(".difficulty").val()
+	    let duration =$(".duration").val()
+	    let startDate = $(".time-from").val()
+	    let finishDate = $(".time-to").val()
+			let capacity = $(".capacity").val()
+			let description = $(".description").val()
+	    let eventObject = {
+				"title": title,
+				"main_sport": sport,
+				"difficulty": difficulty
+				}
+				$.post('https://localhost:3000/api/v1/events/createEvent', eventObject, function(eventObject){
+					console.log(eventObject);
+				})
+	   //preventDefault
+	    // console.log(eventObject)
 
-   let title = $(".title").val()
-    let sport = $(".sport").val()
-    var difficulty = $(".difficulty").val()
-        let duration =$(".duration").val()
-        let stratDate =
-        let finishDate =
-    let userObject = {"title": title, "sport": sport, "difficulty": difficulty}
+	 })
+}
 
-   //preventDefault
-    console.log(userObject)
+function submitForm() {
+	$("form").submit(function(event){
+	    alert("YOU CLICKED IT!")
+	    event.preventDefault();
 
- })
+	    let first_name = $(".first_name").val()
+	    let last_name = $(".last_name").val()
+	    var email = $(".email").val()
+	    let password =$(".password").val()
+	    let personObject = {
+				"first_name": first_name,
+				"last_name": last_name,
+				"email": email,
+				"password":password
+				}
+				$.post('https://localhost:3000/api/v1/events/createPerson', personObject, function(personObject){
+					console.log(perosnObject);
+				})
+	   //preventDefault
+	    // console.log(eventObject)
+
+	 })
+}
