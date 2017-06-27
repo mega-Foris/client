ac01defbc7c300d829f0c41470cb91cdc0afd297
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-06-27 17:07:30 -0600

    adding in Ross' appending information

--- javascript/event_profile.js
+++ javascript/event_profile.js
@@ -6,17 +6,33 @@ $(document).ready(function(){
   getEventInfo(event_id).then(appendEventInfo);
 });//end document ready
 
-
 function getEventInfo(id){
 	let URL = prepareRequest(`api/v1/events/${id}`);
 	return callAPI(URL);
 }
 
-function appendEventInfo(response){
-  console.log(response);
-}
-
 function handleQueryString(queryString){
   let output = parseQueryString(queryString);
   return output.id;
+}
+
+function appendEventInfo(response){
+    $('#location').append(response.city);
+    $('#date').append(response.date_time);
+    $('#activity').append(response.main_sport);
+    $('#difficulty').append(response.difficulty);
+    showAttendees(response.people);
+    showComments(response.comments);
+}
+function showAttendees(people){
+    const source = $('#attendee-template').html();
+    const template = Handlebars.compile(source);
+    const html = template({people});
+    $('.user-facts').append(html);
+}
+function showComments(comment){
+    const source = $('#comment-template').html();
+    const template = Handlebars.compile(source);
+    const html = template({comment});
+    $('.event-comments').append(html);
 }
