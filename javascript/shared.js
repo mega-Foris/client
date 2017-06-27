0d624cd3c705d7e8184c951d98cae5040239572c
Austin <joha0033@gmail.com>
2017-06-27 14:10:20 -0600

    starting post getting from form in shared.js

--- javascript/shared.js
+++ javascript/shared.js
@@ -1,10 +1,24 @@
+const heroku = 'https://forisevents.herokuapp.com/';
+const events_URL = 'api/v1/events';
 
-function prepareRequest(query){
-  const heroku = 'https://forisevents.herokuapp.com/';
-  const route_URL = query;
-  return heroku + route_URL;
+function getEvents(){
+	const URL = heroku + events_URL;
+	return $.get(URL);
 }
 
-function callAPI(URL){
-  return $.get(URL);
-}
+$("form").submit(function(event){
+    //alert("YOU CLICKED IT!"
+    event.preventDefault();
+
+    let title = $(".title").val()
+    let sport = $(".sport").val()
+    var difficulty = $(".difficulty").val()
+		let duration =$(".duration").val()
+		let stratDate =
+		let finishDate = 
+    let userObject = {"title": title, "sport": sport, "difficulty": difficulty}
+
+    //preventDefault
+    console.log(userObject)
+
+  })
