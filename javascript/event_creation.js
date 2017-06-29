5e8110010d174c0085d42d6b9fa433e2ae990d8d
Ross Kiser <kiserr10@gmail.com>
2017-06-29 10:57:37 -0600

    fixed calendar

--- javascript/event_creation.js
+++ javascript/event_creation.js
@@ -2,11 +2,13 @@
 $(document).ready(function(){
   //adds event listener for event creation form
   submitEventForm();
+  $('.modal').modal();
+	$('.datepicker').pickadate({
+	 selectMonths: true, // Creates a dropdown to control month
+	 selectYears: 15 // Creates a dropdown of 15 years to control year
+ });
 
-   $('.modal').modal();
-	 $('input').click(()=>{
-		 $('input').removeClass('validate');
-	 });
+
 });
 
 //event creation
